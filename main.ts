6a66c694f71ab4df0c9fa7275887b059f19177c5
Doug Osborn <d00303905@dmail.dixie.edu>
2021-01-31 20:51:54 -0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,65 @@
+input.onButtonPressed(Button.A, function () {
+    if (colourFlag == 0) {
+        tileDisplay.showColor(Kitronik_Zip_Tile.colors(ZipLedColors.Red))
+    } else if (colourFlag == 1) {
+        tileDisplay.showColor(Kitronik_Zip_Tile.colors(ZipLedColors.Green))
+    } else if (colourFlag == 2) {
+        tileDisplay.showColor(Kitronik_Zip_Tile.colors(ZipLedColors.Blue))
+    } else if (colourFlag == 3) {
+        tileDisplay.showColor(Kitronik_Zip_Tile.colors(ZipLedColors.White))
+    } else if (colourFlag == 4) {
+        tileDisplay.showRainbow(1, 360)
+        rainbowFlag = 1
+    } else if (colourFlag == 5) {
+        tileDisplay.clear()
+        tileDisplay.show()
+        rainbowFlag = 0
+    }
+    if (colourFlag < 5) {
+        colourFlag += 1
+    } else {
+        colourFlag = 0
+    }
+})
+input.onButtonPressed(Button.AB, function () {
+    tileDisplay.clear()
+    for (let Column = 0; Column <= 7; Column++) {
+        for (let Row = 0; Row <= 7; Row++) {
+            tileDisplay.setMatrixColor(Column, Row, Kitronik_Zip_Tile.colors(ZipLedColors.Red))
+            tileDisplay.show()
+            basic.pause(200)
+        }
+    }
+})
+input.onButtonPressed(Button.B, function () {
+    brightLevel += (10 * brightWay)
+    tileDisplay.setBrightness(brightLevel)    
+    if (brightLevel > 241 && brightWay > 0 ) {
+        brightWay = -1
+    } 
+    if (brightLevel < 11 && brightWay < 0) {
+        brightWay = 1   
+    }    
+})
+
+
+let tileDisplay: Kitronik_Zip_Tile.ZIPTileDisplay = null
+let brightLevel = 10
+let brightWay = 1
+let colourFlag = 0
+let rainbowFlag = 0
+rainbowFlag = 1
+colourFlag = 0
+
+tileDisplay = Kitronik_Zip_Tile.createZIPTileDisplay(1, 1, Kitronik_Zip_Tile.UBitLocations.Hidden)
+tileDisplay.setBrightness(brightLevel)
+tileDisplay.clear()
+tileDisplay.showRainbow(1, 360)
+tileDisplay.show()
+basic.forever(function () {
+    if (rainbowFlag == 1) {
+        tileDisplay.rotate(1)
+        tileDisplay.show()
+    }
+})
+
